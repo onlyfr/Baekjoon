b0e2841d69ae36e602b6c119895e90027900a652
onlyfr12 <97716595+onlyfr@users.noreply.github.com>
2026-02-13 16:20:15 +0900

    [Gold IV] Title: 트리의 지름, Time: 0 ms, Memory: 2992 KB -BaekjoonHub

--- 백준/Gold/1967. 트리의 지름/트리의 지름.cc
+++ 백준/Gold/1967. 트리의 지름/트리의 지름.cc
@@ -1,35 +1,28 @@
 #include <bits/stdc++.h>
-#define SZ 10001
+#define SZ 10'001
 using namespace std;
-vector<pair<int, int>> tree[SZ];
-bool chk[SZ];
+constexpr int INF = 1e9;
+vector<pair<int, int>> adj[SZ];
 int V, maxLen, maxNode;
 void init() {
-	maxLen = 0;
-	memset(chk, 0, sizeof(chk));
+    maxLen = 0;
 }
-void dfs(int s, int len) {
-	if (chk[s]) return;
-	chk[s] = true;
-	if (maxLen < len) {
-		maxLen = len; maxNode = s;
-	}
-	for (auto& u : tree[s]) {
-		int v = u.first, w = u.second;
-		dfs(v, len + w);
-	}
+void dfs(int s, int ps, int len) {
+    if (maxLen < len) maxLen = len, maxNode = s;
+    for(auto& [v,w]: adj[s])
+        if (v != ps) dfs(v, s, len+w);
 }
-int main(void) {
-	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-	cin >> V; V--;
-	while (V--) {
-		int u, v, w; cin >> u >> v >> w;
-		tree[u].push_back({ v, w });
-		tree[v].push_back({ u, w });
-	}
-	dfs(1, 0);
-	init();
-	dfs(maxNode, 0);
-	cout << maxLen;
-	return 0;
+int main() {
+    ios::sync_with_stdio(false); cin.tie(NULL);
+    cin >> V; V--;
+    while(V--) {
+        int u, v, w; cin >> u >> v >> w;
+        adj[u].push_back({v,w});
+        adj[v].push_back({u,w});
+    }
+    dfs(1,1,1);
+    init();
+    dfs(maxNode, maxNode, 0);
+    cout << maxLen;
+    return 0;
 }
