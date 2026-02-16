bf2641983202c6c1022b94c8fca94b4cf826519b
onlyfr12 <97716595+onlyfr@users.noreply.github.com>
2026-02-16 16:23:29 +0900

    [Platinum V] Title: LCA 2, Time: 72 ms, Memory: 16872 KB -BaekjoonHub

---
+++ 백준/Platinum/11438. LCA 2/LCA 2.cc
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+constexpr int SZ = 100'001;
+constexpr int LOG = 17;
+int N, M, parent[SZ][LOG], depth[SZ];
+vector<int> adj[SZ];
+void dfs(int s, int ps, int d) {
+    depth[s] = d;
+    for(auto& u: adj[s]){
+        if (u!=ps) {
+            parent[u][0] = s;
+            dfs(u, s, d+1);
+        }
+    }
+}
+void setParent() {
+    for(int k=1; k<LOG; ++k)
+        for(int i=1; i<=N; ++i)
+            if(parent[i][k-1] != 0) parent[i][k] = parent[parent[i][k-1]][k-1];
+}
+int lca(int u, int v) {
+    if(depth[v] > depth[u]) swap(u,v);
+    for(int k=LOG-1; k>=0; --k)
+        if(depth[u]-depth[v] >= (1<<k)) u = parent[u][k];
+    if (u==v) return u;
+    for(int k=LOG-1; k>=0; --k)
+        if(parent[u][k] != parent[v][k]) u = parent[u][k], v = parent[v][k];
+    return parent[u][0];
+}
+int main() {
+    ios::sync_with_stdio(false); cin.tie(NULL); 
+    int u, v; cin >> N; 
+    for(int i=0; i<N-1; ++i){
+        cin >> u >> v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+    dfs(1, 1, 0);
+    setParent();
+    cin >> M;
+    while(M--) {
+        cin >> u >> v;
+        cout << lca(u, v) << '\n';
+    }
+    return 0;
+}
